7a85b8cf34364a00e0de19bfa65324974e7c05e4
yagi17 <nafissadik785@gmail.com>
2024-04-26 17:53:21 +0600

    connet to mongo db and added environmental veriable

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -7,11 +9,45 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+// connect to mongo db 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5bvaa0x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.get('/', async(req, res)=>{
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const dbConnect = async () => {
+    try {
+        await client.db("admin").command({ ping: 1 });
+        console.log("You successfully connected to MongoDB!");
+    } catch (error) {
+        console.log(error)
+
+    }
+}
+dbConnect()
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', async (req, res) => {
     res.send('Travel Europe server in running')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log((`Travel Europe server in running on ${port}`));
 })
